42d13a0948dee7d84bb0820a0fb11a5ba58c928a
zzangjyj0818 <jyj010818@naver.com>
2023-05-14 18:22:59 +0900

    new

---
+++ C_Assignment/C_1-1-1/C_1-1-1/C_1-1-1.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Num1 */
+/*
+int main(){
+    double dollar;
+    double n;
+    
+    printf("환율(원/달러) : ");
+    scanf("%lf", &dollar);
+    
+    printf("환전할 금액(원) : ");
+    scanf("%lf", &n);
+    
+    printf("%lf원을 환전한 금액은 %.1lf 달러입니다.", n, n/dollar);
+}
+*/
+
+/* Num2 */
+/*
+int main(){
+    int a,b;
+    
+    printf("2개의 정수를 입력하세요 : ");
+    scanf("%d%d", &a, &b);
+    
+    printf("%d + (%d) = %d\n", a,b,a+b);
+    printf("%d - (%d) = %d\n", a,b,a-b);
+    printf("%d * (%d) = %d\n", a,b,a*b);
+    printf("%d / (%d) = %d\n", a,b,a/b);
+    printf("%d %% (%d) = %d\n", a,b,a%b);
+}
+*/
+
+/* Num3 */
+
+/*
+int main(){
+    double a,b,c;
+    printf("친구 3명의 키(cm) : ");
+    scanf("%lf%lf%lf", &a,&b,&c);
+    
+    printf("평균 키(cm) : %.1lf", (a+b+c)/3);
+}
+*/
+
+/* Num4 */
+/*
+int main(){
+    int c;
+    printf("온도(C)를 입력하세요: ");
+    scanf("%d", &c);
+    
+    printf("화씨(F)온도는 %.0lf도입니다.", 1.8*c+32);
+}
+*/
+
+/* Num5 */
+/*
+int main(){
+    int snack, drink;
+    
+    printf("과자 판매액(만원) : ");
+    scanf("%d", &snack);
+    
+    printf("음료수 판매액(만원) : ");
+    scanf("%d", &drink);
+    
+    printf("오늘의 수익(만원) : %.0lf", 0.15*snack + 0.1*drink);
+}
+*/
+
+/* Num6 */
+/*
+int main(){
+    double sec;
+    
+    printf("철수가 달린 초(second) : ");
+    scanf("%lf", &sec);
+    
+    printf("철수가 달린 거리(m) : %.1lf", 10000*(sec/3600));
+}
+*/
+
+/* Num7 */
+/*
+int main(){
+    //월급을 받을 실수형 변수 선언
+    double money;
+    
+    printf("월급(만원): ");
+    scanf("%lf", &money);
+    
+    printf("연봉은 %.0lf(만원)입니다.\n", money*12);
+    printf("세금은 %.5lf(만원)입니다.\n", (money*0.2)*12);
+}
+*/
+
+/* Num8 */
+/*
+int main(){
+    char c;
+    printf("문자 : ");
+    scanf("%c", &c);
+    
+    printf("입력한 문자 %c의 ASCII code값: %d", c, c);
+}
+*/
+
+/* Num9 */
+/*
+int main(){
+    printf("문자형 변수크기(바이트): char - %d\n", (int)sizeof(char));
+    printf("정수형 변수크기(바이트): short - %d\t int - %d\t long - %d\n", (int)sizeof(short), (int)sizeof(int), (int)sizeof(long));
+    printf("실수형 변수크기(바이트): float - %d\t double - %d\t long double - %d\n", (int)sizeof(float), (int)sizeof(double), (int)sizeof(long double));
+}
+*/
+
+/* Num10 */
+/*
+int main(){
+    int a;
+    short b;
+    
+    printf("short 변수에 저장할 값: ");
+    scanf("%d", &b);
+    
+    printf("int 변수에 저장할 값: ");
+    scanf("%d", &a);
+    
+    printf("short 변수에 실제 저장된 값: %d", b);
+    printf("int 변수에 실제 저장된 값: %d", a);
+}
+*/
---
+++ C_Assignment/C_1-1-2/C_1-1-2/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Num1 */
+/*
+int main(){
+    printf("%d : %o : %x \n", 10,10,10);
+}
+*/
+
+/* Num2 */
+/*
+int main(){
+    double speed, h, m, s;
+    printf("시속(km/h) : ");
+    scanf("%lf", &speed);
+    
+    printf("운전시간(시,분,초) : ");
+    scanf("%lf%lf%lf", &h ,&m, &s);
+    
+    double distance = speed*h+(speed/60)*m+ (speed/3600)*s;
+    printf("달린 거리(km) : %.1f\n", distance);
+}
+*/
+
+/* Num3 */
+/*
+int main(){
+    char c;
+    printf("문자를 입력하세요 : ");
+    scanf("%c", &c);
+    
+    for(int i = 1; i<4; i++){
+        printf("문자: %c\tASCII 코드 : %d(십진수) %x(16진수)\n", c+i, c+i, c+i);
+    }
+}
+*/
+
+/* Num4 */
+
+/* Num5 */
+/*
+int main(){
+    double kr, en, ma;
+    printf("국 영 수 : ");
+    scanf("%lf%lf%lf", &kr, &en, &ma);
+    
+    printf("=========================================================\n");
+    printf("|    국어    |    영어    |    수학    |    총점    |    평균   |\n");
+    printf("=========================================================\n");
+    printf("|    %.0lf    |    %.0lf    |    %.0lf    |    %.0lf    |    %.1lf   |\n", kr,en,ma, kr+en+ma, (kr+en+ma)/3);
+    printf("=========================================================\n");
+
+}
+*/
